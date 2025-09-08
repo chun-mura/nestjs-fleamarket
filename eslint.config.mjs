a4cd2224aee53f6a7904abbee89094950a400533
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2025-09-08 23:29:29 +0900

    fix: eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,4 +31,12 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'warn'
     },
   },
+  {
+    files: ['**/*.dto.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off'
+    },
+  },
 );
